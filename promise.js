f40736883e412a378b112f9944a42449a4c3095f
MegumiOhara <156937802+MegumiOhara@users.noreply.github.com>
2024-10-09 09:36:29 +0900

    clean up spaces

--- promise.js
+++ promise.js
@@ -35,7 +35,10 @@ const randomNumber = () => {
 // Task 2.2 Fetching data from API with async/await
 const fetchAdviceById = async (id) => {
     try {
-        const response = await fetch(`https://api.adviceslip.com/advice/${id}`)
+        const response = await fetch(`https://api.adviceslip.com/advice/${id}`);
+        if(!response.ok){
+            throw new Error("Network response was not ok");
+        }
         const data = await response.json();
         const advice = data.slip.advice;
         document.getElementById('advice').innerText= `You got a ${id}, your advice: ${advice}`;
